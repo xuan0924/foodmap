3f2c42bfd74aede7429cd4e93012c825b2c7c7cd
张一轩 <xuan@zhangyixuandeMacBook-Air.local>
2026-04-02 09:51:44 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 window.AMAP_CONFIG = {
     KEY: "9e003dbbac324e4fbb9df5874b7d8e57",
-    SECURITY_CODE: "050e22e7945533547df0a66b0fdc5928",
+    SECURITY_CODE: "050e227945533547df0a66b0fdc5928",
     CENTER: [104.195397, 35.86166], // 全国视图大致中心
     DEFAULT_ZOOM: 5,
     /** 打开页面时默认搜索城市（可改为你常去的城市） */
